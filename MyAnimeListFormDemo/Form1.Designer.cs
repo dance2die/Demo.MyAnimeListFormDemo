697cf86d66806efc8b3095e72b9b562dab457cc1
dance2die <dontbothermewithspam@gmail.com>
2017-01-29 16:26:31 -0500

    added my anime list sharp.

--- MyAnimeListFormDemo/Form1.Designer.cs
+++ MyAnimeListFormDemo/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Location = new System.Drawing.Point(13, 37);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(238, 20);
+            this.tbSearch.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSearch);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.Button button1;
     }
 }
 
